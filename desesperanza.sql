fd34f5a9738a17d5de1ffbca66f1d4ac3ccb342c
cesar eduardo hernandez hernandez <161785004+chicharo0010@users.noreply.github.com>
2024-10-27 23:15:21 -0600

    back

---
+++ desesperanza.sql
@@ -0,0 +1,43 @@
+create database desesperanza;
+use desesperanza;
+create table clientes(
+id_usuario int auto_increment primary key,
+nombre varchar(50) not null,
+correo varchar(50) not null
+);
+create table panes(
+id_pan int auto_increment primary key,
+nombre_pan varchar(30) not null,
+/*ingrredientes varchar(100),*/
+cantidad int not null,
+precio_pan int not null
+);
+create table facturas(
+id_factura  int auto_increment primary key,
+id_usuario int not null,
+total int not null,
+rfc varchar(15),
+foreign key (id_usuario) references clientes (id_usuario)
+);
+ create table Productos_Facturas(
+ id_factura  int not null,
+ id_pan  int not null,
+ cantidad int not null,
+ foreign key (id_factura) references facturas (id_factura),
+ foreign key (id_pan) references panes (id_pan)
+ );
+create table pedidos(
+id_pedido int auto_increment primary key,
+id_usuario int not null,
+total int not null, 
+foreign key (id_usuario) references clientes (id_usuario)
+);
+
+create table prdts_pedidos (
+id_pedido int not null,
+id_pan int not null,
+cantidad int not null,
+foreign key (id_pedido) references pedidos (id_pedido),
+foreign key (id_pan) references panes (id_pan)
+);
+
